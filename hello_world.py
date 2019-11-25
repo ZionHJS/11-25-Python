1dd0d03dd5f5e6c3be9e322640cd36587a77645b
zionHuang <hjszion@gmail.com>
2019-11-25 15:20:04 -0800

    update

--- hello_world.py
+++ hello_world.py
@@ -1,35 +1,27 @@
-print('Hello World')
-
-name = "Zen"
-print('My name is', name)
-print('My name is' + name)
-
-first_name = 'Zen'
-last_name = 'coder'
-age = 27
-print(f'My name is {first_name} {last_name} and I am {age} years old.')
+# 1. TASK: print "Hello World"
+print( 'Hello World' )
+# 2. print "Hello Noelle!" with the name in a variable
+name = "Noelle"
+print( 'Hello', name )	# with a comma
+print( 'Hello' + name)	# with a +
+# 3. print "Hello 42!" with the number in a variable
+name = 42
+print( 'Hello', name' )	# with a comma
+print( 'Hello' + name )	# with a +	-- this one should give us an error!
+# 4. print "I love to eat sushi and pizza." with the foods in variables
+fave_food1 = "sushi"
+fave_food2 = "pizza"
+print( f'I love eat {fave_food1} and {fave_food2}.') # with an f string
+print( 'I love to eat {} and {}'.format(fave_food1, fave_food2)) # with .format()
 
 first_name = "Zen"
 last_name = "Coder"
 age = 27
-print("My name is {} {} and I am {} years old.".format(first_name, last_name, age))
-# output: My name is Zen Coder and I am 27 years old.
-print("My name is {} {} and I am {} years old.".format(age, first_name, last_name))
-# output: My name is 27 Zen and I am Coder years old.
+print(f"My name is {first_name} {last_name} and I am {age} years old.")
 
-#%-formatting
-hw = "Hello %s" % "world" 	# with literal values
-py = "I love Python %d" % 3 
-print(hw, py)
-# output: Hello world I love Python 3
-name = "Zen"
-age = 27
-print("My name is %s and I'm %d" % (name, age))		# or with variables
-# output: My name is Zen and I'm 27
-
-x = "hello world"
-y = "this title"
-print(x.title(), y.title())
-# output:
-("Hello World" , "this title")
-
+#code block
+x = 10
+if x > 50:
+	print("bigger than 50")
+else:
+	print("smaller than 50")
