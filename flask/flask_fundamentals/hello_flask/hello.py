365cb8c88613da86b102efa1a3ae751162829186
zionHuang <hjszion@gmail.com>
2019-11-26 18:20:31 -0800

    update

--- flask/flask_fundamentals/hello_flask/hello.py
+++ flask/flask_fundamentals/hello_flask/hello.py
@@ -1,10 +1,25 @@
 # This will be the "server" file where we will set up all of our routes to handle requests
 
-from flask import Flask  # Import Flask to allow us to create our app
+from flask import Flask  # Import Flask class to allow us to create our app
 app = Flask(__name__)    # Create a new instance of the Flask class called "app"
 @app.route('/')          # The "@" decorator associates this route with the function immediately following
 def hello_world():
     return 'Hello World!'  # Return the string 'Hello World!' as a response
 if __name__=="__main__":   # Ensure this file is being run directly and not from a different module    
     app.run(debug=True)    # Run the app in debug mode.
+#change from paths
+@app.route('/success')
+def success():
+  return "success"
 
+#variable rules
+@app.route('/hello/<name>')
+def hello(name):
+    print(name)
+    return "Hello, " + name
+
+@app.route('/users/<username>/<id>')
+def show_user_profile(username, id):
+    print(username)
+    print(id)
+    return "username: " + username + ", id" + id
