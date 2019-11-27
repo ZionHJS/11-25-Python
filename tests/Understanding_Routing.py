53f2d3892e5b26186889704eeb124db46092733c
zionHuang <hjszion@gmail.com>
2019-11-26 18:35:22 -0800

    update

--- tests/Understanding_Routing.py
+++ tests/Understanding_Routing.py
@@ -3,26 +3,34 @@
 from flask import Flask  # Import Flask class to allow us to create our app
 app = Flask(__name__)    # Create a new instance of the Flask class called "app"
 
+# '/' have it say "Hello World!"
 @app.route('/')          # The "@" decorator associates this route with the function immediately following
 def hello_world():
     return 'Hello World!'  # Return the string 'Hello World!' as a response
 
+# '/dojo' have it say "Dojo!"
+@app.route('/dojo')
+def dojo():
+    return "Dojo!"
+
+# '/say/<name>' have it say Hi <name>
+@app.route('/say/<name>')
+def say(name):
+    return "Hello" + name
+
+# '/repeat/<num>/<name>' have it say "name" *num times
+@app.route('/repeat/<num>/<name>')
+def repeat(num, name):
+    sum = name
+    for x in range(0, num):
+        sum = sum + name
+    return sum
+ 
 #change from paths
 @app.route('/success')
 def success():
   return "success"
 
-#variable rules
-@app.route('/hello/<name>')
-def hello(name):
-    print(name)
-    return "Hello, " + name
-
-@app.route('/users/<username>/<id>')
-def show_user_profile(username, id):
-    print(username)
-    print(id)
-    return "username: " + username + ", id" + id
 
 if __name__=="__main__":   # Ensure this file is being run directly and not from a different module    
     app.run(debug=True)    # Run the app in debug mode.
