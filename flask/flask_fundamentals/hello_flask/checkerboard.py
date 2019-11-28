0b76072a1b5ff9a51fe64ea0b85edf5e3aecfcbc
zionHuang <hjszion@gmail.com>
2019-11-27 16:07:43 -0800

    update

--- flask/flask_fundamentals/hello_flask/checkerboard.py
+++ flask/flask_fundamentals/hello_flask/checkerboard.py
@@ -3,10 +3,9 @@
 from flask import Flask, render_template  # Import Flask class to allow us to create our app
 app = Flask(__name__)    # Create a new instance of the Flask class called "app"
 
-@app.route('/')          # The "@" decorator associates this route with the function immediately following
-def index():
-    return render_template("index.html", phrase="hello", times=5)   #notice the 2 new arguments
-
+@app.route('/<x>/<y>')          # The "@" decorator associates this route with the function immediately following
+def checkerboard(x, y):
+    return render_template('checkerboard.html', rows=int(x), cols=int(y))
 
 if __name__=="__main__":   # Ensure this file is being run directly and not from a different module    
     app.run(debug=True)    # Run the app in debug mode.
