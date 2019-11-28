79a152d0e974c99b9b9b23722cce44da831a6854
zionHuang <hjszion@gmail.com>
2019-11-27 18:34:38 -0800

    update

--- flask/flask_fundamentals/hello_flask/server.py
+++ flask/flask_fundamentals/hello_flask/server.py
@@ -10,7 +10,7 @@ def index():
 @app.route('/users', methods=['POST'])
 def create_user():
     print('Got Post Info')
-    print(request.form)
+    print(request.form)   #request.form dictionary
     name_from_form = request.form['name']
     email_from_form = request.form['email']
     return render_template('show.html', name_on_template=name_from_form, email_on_template=email_from_form)
