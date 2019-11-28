f0db14226405c1fee1d41e88540e08212ab53807
zionHuang <hjszion@gmail.com>
2019-11-27 18:07:25 -0800

    update

--- flask/flask_fundamentals/hello_flask/html_table.py
+++ flask/flask_fundamentals/hello_flask/html_table.py
@@ -5,13 +5,13 @@ app = Flask(__name__)    # Create a new instance of the Flask class called "app"
 
 @app.route('/table')
 def htmltable():
-    users = [
+    user_info = [
         {'first_name':'Michael', 'last_name':'Choi'},
         {'first_name':'John', 'last_name':'Supsupin'},
         {'first_name':'Mark', 'last_name':'Guillen'},
         {'first_name':'KB', 'last_name':'Tonel'}
     ]
-    return render_template('table.html', users=users)
+    return render_template('table.html', users=user_info)
 
 if __name__=="__main__":   # Ensure this file is being run directly and not from a different module    
     app.run(debug=True)    # Run the app in debug mode.
