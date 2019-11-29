9dc0e0c7de1528a417c06e01582731f0922cbf79
zionHuang <hjszion@gmail.com>
2019-11-28 16:39:30 -0800

    update

--- flask/flask_fundamentals/hello_flask/redirect.py
+++ flask/flask_fundamentals/hello_flask/redirect.py
@@ -1,15 +1,16 @@
-from flask import Flask, render_template, request, redirect
-app = Flask(__name__)
-
+from flask import Flask, render_template, request, redirect # don't forget to import redirect!
+    
 @app.route('/users', methods=['POST'])
 def create_user():
+    print("Got Post Info")
     print(request.form)
     name = request.form['name']
-    email = request.from['email']
-    return redirect('/show') #changed this line
-
-#adding this method
-@app.route('/show')
+    email = request.form['email']
+    return redirect("/show")	# changed this line!
+    
+# adding this method
+@app.route("/show")
 def show_user():
+    print("Showing the User Info From the Form")
     print(request.form)
-    return render_template('show.html')
+    return render_template("show.html")
