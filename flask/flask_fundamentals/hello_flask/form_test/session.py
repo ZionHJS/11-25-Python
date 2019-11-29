8a462130b59a4738359c5c0ae2efde22b41ca6e5
zionHuang <hjszion@gmail.com>
2019-11-28 16:51:03 -0800

    update

---
+++ flask/flask_fundamentals/hello_flask/form_test/session.py
@@ -0,0 +1,15 @@
+from flask import Flask, render_template, request, redirect, session
+app = Flask(__name__)
+app.secret_key = 'keep it secret, keep it safe' # set a secret key for security pruposes
+
+@app.route('/users', methods=['POST'])
+def create_user():
+    print('Got Post Info')
+    session['username'] = request.form['name']
+    session['useremail'] = request.form['email']
+    return redirect('/show')
+
+@app.route('/show')
+def show_user():
+    return render_template('show.html', name_on_template=session['username'], email_on_template=session['useremail'])
+
