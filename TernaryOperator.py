db4347eab249070c8ec9b6b43209fa398d2736b0
zionHuang <hjszion@gmail.com>
2019-12-01 00:13:11 -0800

    update

--- TernaryOperator.py
+++ TernaryOperator.py
@@ -6,5 +6,12 @@ def square(num):
     return x
     
 #turn it into anonymous(lambda) function
-lambda num: num ** 2
+lambda num: num ** 2   #This means "here is an anonymous (nameless) function that takes one argument, called num, and returns num**2.
+
+#a anonymous function that takes two arguments and returns the sum of the two arguments
+lambda num1, num2: num1 + num2
+
+#define a function and unnecessarily consume memory and complicate our code, just to produce the same result:
+my_arr = [1,2,3,4,5]
+print(list(map(lambda x: x**2, my_arr)))
 
