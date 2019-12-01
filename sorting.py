5961a10ad8b7ad97cd9b669c8523496f45ad6db7
zionHuang <hjszion@gmail.com>
2019-11-30 23:51:32 -0800

    update

--- sorting.py
+++ sorting.py
@@ -5,6 +5,7 @@ arr[0], arr[1] = arr[1], arr[0]
 # bubble sort
 arr = [8, 3, 6, 2, 4, 1, 7, 5]
 
+
 def bubbleSort(arr):
     for j in range(len(arr)):
         for i in range(1, len(arr)-j):
@@ -12,16 +13,20 @@ def bubbleSort(arr):
                 arr[i-1], arr[i] = arr[i], arr[i-1]
     return arr
 
+
 print(bubbleSort(arr))
 
 
-#selection sort
+# selection sort
 arr = [8, 3, 6, 2, 4, 1, 7, 5]
+
 def selectionSort(arr):
-    min = arr[0]
-    index = 0
-    for i in range(len(arr)):
-        if min > arr[i]:
-            min = arr[i]
-            index = i
-    
+    for j in range(len(arr)):
+        min = j
+        for i in range(j, len(arr)):
+            if arr[min] > arr[i]:
+                min = i
+        arr[j], arr[min] = arr[min], arr[j]
+    return arr
+print(selectionSort(arr))
+
---
+++ tests/InsertionSort.py
@@ -0,0 +1,3 @@
+#insertionSort
+def insertSort():
+    
