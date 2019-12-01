8cce4b8b67bb1a30bae06b6c37d08a828a37abd9
zionHuang <hjszion@gmail.com>
2019-12-01 00:05:56 -0800

    update

---
+++ TernaryOperator.py
@@ -0,0 +1,10 @@
+<result_if_true> if <condition> else<result_if_false>
+
+#Lambdas
+def square(num):
+    x = num ** 2
+    return x
+    
+#turn it into anonymous(lambda) function
+lambda num: num ** 2
+
--- tests/InsertionSort.py
+++ tests/InsertionSort.py
@@ -1,3 +1,10 @@
 #insertionSort
-def insertSort():
-    
+arr = [8, 3, 6, 2, 4, 1, 7, 5]
+def insertSort(arr):
+    for i in range(1, len(arr)):
+        for j in range(i-1, -1, -1):
+            if arr[j] > arr[j+1]:
+                arr[j], arr[j+1] = arr[j+1], arr[j]
+    return arr
+print(insertSort(arr))
+
