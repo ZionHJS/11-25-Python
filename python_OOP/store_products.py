4f0ac0d37751146d93e0cfa1bed6515ad0a65812
zionHuang <hjszion@gmail.com>
2019-12-02 12:21:16 -0800

    update

--- python_OOP/store_products.py
+++ python_OOP/store_products.py
@@ -1,7 +1,15 @@
 class store:
     def __init__(self, name, list_products):
         self.name = name
-        self.list_products = ''
+        self.list_products = []
+    def add_products(self, new_product):
+        self.list_products.append(new_product)
+    def sell_product(self, id):
+        self.list_products.remove(list_products[id])
+    def inflation(self, percent_increase):
+    
+    def set_clearance(self, category, percent_discount):
+        
     
 class product:
     def __init__(self, price, category):
@@ -12,3 +20,5 @@ class product:
             self.price = self.price*(1+percent_change)
         else:
             self.price = self.price*(1-percent_change)
+    def print_info(self):
+        print(f'{self.name}', f'{self.category}', f'{self.price}')
