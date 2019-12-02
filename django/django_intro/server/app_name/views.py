dcf837abc35f73b3e192317be245671ec0eb9c04
zionHuang <hjszion@gmail.com>
2019-12-02 13:16:23 -0800

    update

--- django/django_intro/server/app_name/views.py
+++ django/django_intro/server/app_name/views.py
@@ -1,5 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import render, HttpResponse
 
 # Create your views here.
 def index(request):
-    return render(request, 'index.html')
+    return HttpResponse("this is the equivalent of @app.rout('/')!")
+    #return render(request, 'index.html')
