b5756c0c5d7098eb3ffeacb58a2815438e8d549b
zionHuang <hjszion@gmail.com>
2019-12-03 12:38:30 -0800

    update

--- django/django_intro/assignment/NinjaGold/_ninja_gold/urls.py
+++ django/django_intro/assignment/NinjaGold/_ninja_gold/urls.py
@@ -3,6 +3,9 @@ from . import views
 
 urlpatterns = [
     path('', views.index),
-    path('process_money', views.process)
+    path('process_farm', views.process_farm),
+    path('process_cave', views.process_cave),
+    path('process_house', views.process_house),
+    path('process_casino', views.process_casino)
 ]
 
--- django/django_intro/assignment/NinjaGold/_ninja_gold/views.py
+++ django/django_intro/assignment/NinjaGold/_ninja_gold/views.py
@@ -1,11 +1,10 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 import random
 import datetime
 
 def randomNum(start, end):
-    num = random.randrage(start, end)
+    num = random.randrange(start, end)
     return num
-
 def earnOrAdd():
     chance = randomNum(0,2)
     if change == 1:
@@ -13,11 +12,31 @@ def earnOrAdd():
     else:
         return False
 
-
-
 # Create your views here.
 def index(request):
     return render(request, 'index.html')
 
-def process(request):
-    
+def process_farm(request):
+    farmNum = randomNum(10,20)
+    request.session['total_gold'] += farmNum
+    return redirect('/')
+
+def process_cave(request):
+    caveNum = randomNum(5,10)
+    request.session['total_gold'] += caveNum
+    return redirect('/')
+
+def process_house(request):
+    houseNum = randomNum(2,5)
+    request.session['total_gold'] += houseNum
+    return redirect('/')
+
+def process_casino(request):
+    casinoNum = randomNum(0,50)
+    chance = earnOrAdd()
+    if chance == True:
+        request.session['total_gold'] += casinoNum
+    else:
+        request.session['total_gold'] -= casinoNum
+    return redirect('/')
+
