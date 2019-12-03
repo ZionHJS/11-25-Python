2e95ca74763c419926f2f20cecb6468b6196e368
zionHuang <hjszion@gmail.com>
2019-12-03 12:22:02 -0800

    update

--- django/django_intro/assignment/NinjaGold/_ninja_gold/views.py
+++ django/django_intro/assignment/NinjaGold/_ninja_gold/views.py
@@ -2,6 +2,19 @@ from django.shortcuts import render
 import random
 import datetime
 
+def randomNum(start, end):
+    num = random.randrage(start, end)
+    return num
+
+def earnOrAdd():
+    chance = randomNum(0,2)
+    if change == 1:
+        return True
+    else:
+        return False
+
+
+
 # Create your views here.
 def index(request):
     return render(request, 'index.html')
