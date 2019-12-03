3610f9b351f086b21eaa465f85385fcd05d98d9a
zionHuang <hjszion@gmail.com>
2019-12-02 23:11:20 -0800

    update

--- django/django_intro/assignment/random_word_generator/random_word/views.py
+++ django/django_intro/assignment/random_word_generator/random_word/views.py
@@ -3,3 +3,8 @@ from django.shortcuts import render
 # Create your views here.
 def index(request):
     return render(request, 'index.html')
+
+def generate(request):
+    count = 0
+    request.session['num'] = count+1
+    
