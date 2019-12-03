1a5cf30872838b0274c976500c99163ecbc71a70
zionHuang <hjszion@gmail.com>
2019-12-03 13:33:56 -0800

    update

--- django/django_intro/assignment/NinjaGold/_ninja_gold/urls.py
+++ django/django_intro/assignment/NinjaGold/_ninja_gold/urls.py
@@ -3,9 +3,6 @@ from . import views
 
 urlpatterns = [
     path('', views.index),
-    path('process_farm', views.process_farm),
-    path('process_cave', views.process_cave),
-    path('process_house', views.process_house),
-    path('process_casino', views.process_casino)
+    path('process_money', views.process_money),
 ]
 
--- django/django_intro/assignment/NinjaGold/_ninja_gold/views.py
+++ django/django_intro/assignment/NinjaGold/_ninja_gold/views.py
@@ -5,38 +5,64 @@ import datetime
 def randomNum(start, end):
     num = random.randrange(start, end)
     return num
+
 def earnOrAdd():
     chance = randomNum(0,2)
-    if change == 1:
+    if chance == 1:
         return True
     else:
         return False
 
+def addActivity(request, num, action, location):
+    timestamp = datetime.datetime.now()
+    if location == 'casino':
+        if action == 'earned':
+            earned = 'Earned %d from the casino! %s' % (num, timestamp)
+            request.session['activity'].append(['earn', earned])
+        elif action == 'lost':
+            lost = 'Enterd acasino and lost %d gold... Ouch %s' % (num, timestamp)
+            request.session['activity'].append(['lost', lost])
+        else:
+            print('error')
+    elif location == 'farm':
+        request.session['activity'].append(['earn', 'Earned %d from the %s! %s' % (num, location, timestamp)])
+    elif location == 'cave':
+        request.session['activity'].append(['earn', 'Earned %d from the %s! %s' % (num, location, timestamp)])
+    elif location == 'house':
+        request.session['activity'].append(['earn', 'Earned %d from the %s! %s' % (num, location, timestamp)])
+    else:
+        print('error')
+
 # Create your views here.
 def index(request):
+    if request.session['total'] == None:
+        request.session['total'] = 0
+    if request.session['activities'] == None:
+        request.session['activities'] = []
     return render(request, 'index.html')
 
-def process_farm(request):
-    farmNum = randomNum(10,20)
-    request.session['total_gold'] += farmNum
-    return redirect('/')
-
-def process_cave(request):
-    caveNum = randomNum(5,10)
-    request.session['total_gold'] += caveNum
-    return redirect('/')
-
-def process_house(request):
-    houseNum = randomNum(2,5)
-    request.session['total_gold'] += houseNum
-    return redirect('/')
-
-def process_casino(request):
-    casinoNum = randomNum(0,50)
-    chance = earnOrAdd()
-    if chance == True:
-        request.session['total_gold'] += casinoNum
-    else:
-        request.session['total_gold'] -= casinoNum
+def process_money(request):
+    hiddenInput = request.form['hidden']
+    if hiddenInput == 'farm':
+        farmNum = randomNum(10,20)
+        request.session['total_gold'] += farmNum
+        addActivity(farmNum, 'earned', 'farm')
+    elif hiddenInput == 'cave':
+        caveNum = randomNum(5,10)
+        request.session['total_gold'] += caveNum
+        addActivity(caveNum, 'earned', 'cave')
+    elif hiddenInput == 'house':
+        houseNum = randomNum(2,5)
+        request.session['total_gold'] += houseNum
+        addActivity(houseNum, 'earned', 'house')
+    elif hiddenInput == 'casino':
+        casinoNum = randomNum(0,50)
+        chance = earnOrAdd()
+        if chance:
+            request.session['total_gold'] += casinoNum
+            addActivity(casinoNum, 'earned', 'casino')
+        else:
+            request.session['total_gold'] -= casinoNum
+            addActivity(casinoNum, 'lost', 'casino')
     return redirect('/')
 
