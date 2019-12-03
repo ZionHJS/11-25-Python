cb65e21b67297db6c37493a6c62a4488d6daf8d1
zionHuang <hjszion@gmail.com>
2019-12-02 23:13:38 -0800

    update

--- django/django_intro/assignment/random_word_generator/random_word/views.py
+++ django/django_intro/assignment/random_word_generator/random_word/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from django.utils.crypto import get_random_string
 
 # Create your views here.
 def index(request):
@@ -6,5 +7,6 @@ def index(request):
 
 def generate(request):
     count = 0
-    request.session['num'] = count+1
+    request.session['count'] = count+1
+    random_str = get_random_string(length=14)
     
