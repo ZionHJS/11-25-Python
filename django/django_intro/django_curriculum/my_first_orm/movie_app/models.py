d9932bf96afe3108e6ef422cd33fdd233dfa30db
zionHuang <hjszion@gmail.com>
2019-12-03 18:56:41 -0800

    update

--- django/django_intro/django_curriculum/my_first_orm/movie_app/models.py
+++ django/django_intro/django_curriculum/my_first_orm/movie_app/models.py
@@ -9,3 +9,9 @@ class Movie(models.Model):
     duration = models.IntegerField()
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
+
+class Wizard(models.Model):
+    name = models.CharField(max_length=45)
+    house = models.CharField(max_length=45)
+    pet = models.CharField(max_length=45)
+    year = models.IntegerField()
