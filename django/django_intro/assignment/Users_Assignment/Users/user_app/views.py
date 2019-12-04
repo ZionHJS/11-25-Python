5571248dcabab9c19d7192fcc37a7a44ca4ba34a
zionHuang <hjszion@gmail.com>
2019-12-04 15:57:03 -0800

    update

--- django/django_intro/assignment/Users_Assignment/Users/user_app/views.py
+++ django/django_intro/assignment/Users_Assignment/Users/user_app/views.py
@@ -1,9 +1,7 @@
 from django.shortcuts import render
-from . import User
-
 # Create your views here.
 def index(request):
     context = {
-        'all_the_users':User.objects.all()
+        #'all_the_users':User.objects.all()
     }
     return render(request, 'index.html')
