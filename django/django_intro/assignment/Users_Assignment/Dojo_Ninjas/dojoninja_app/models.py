4ee037c5f70426212d41734eb9195ca63eee9b86
zionHuang <hjszion@gmail.com>
2019-12-04 17:08:06 -0800

    update

--- django/django_intro/assignment/Users_Assignment/Dojo_Ninjas/dojoninja_app/models.py
+++ django/django_intro/assignment/Users_Assignment/Dojo_Ninjas/dojoninja_app/models.py
@@ -5,6 +5,7 @@ class Dojo(models.Model):
     name = models.CharField(max_length=255)
     city = models.CharField(max_length=255)
     state = models.CharField(max_length=2)
+    desc = models.CharField(max_length=255)
     create_at = models.DateTimeField(auto_now_add=True)
     update_at = models.DateTimeField(auto_now=True)
 
