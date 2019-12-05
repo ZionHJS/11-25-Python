86e3a007c35d532879b4333145c15281e620a07c
zionHuang <hjszion@gmail.com>
2019-12-04 18:52:17 -0800

    update

--- django/django_intro/assignment/Users_Assignment/books_authors/books_authors_proj/books_authors_app/models.py
+++ django/django_intro/assignment/Users_Assignment/books_authors/books_authors_proj/books_authors_app/models.py
@@ -10,6 +10,7 @@ class Book(models.Model):
 class Author(models.Model):
     first_name = models.CharField(max_length=45)
     last_name = models.CharField(max_length=45)
+    notes = models.CharField(max_length=255, default="note")
     books = models.ManyToManyField(Book, related_name='publishers')
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
