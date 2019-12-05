6d9d8c8042ee4052fb777d2785305cf65b1bac00
zionHuang <hjszion@gmail.com>
2019-12-05 10:24:15 -0800

    update

--- django/django_intro/assignment/Users_Assignment/Dojo_Ninjas/dojoninja_app/views.py
+++ django/django_intro/assignment/Users_Assignment/Dojo_Ninjas/dojoninja_app/views.py
@@ -1,5 +1,9 @@
 from django.shortcuts import render
+from .models import Dojo
 
 # Create your views here.
 def index(request):
-    return render(request, 'index.html')
+    context = {
+        'all_dojos':Dojo.objects.all()
+    }
+    return render(request, 'index.html', context)
--- django/django_intro/assignment/Users_Assignment/books_authors/books_authors_proj/books_authors_app/models.py
+++ django/django_intro/assignment/Users_Assignment/books_authors/books_authors_proj/books_authors_app/models.py
@@ -11,6 +11,7 @@ class Author(models.Model):
     first_name = models.CharField(max_length=45)
     last_name = models.CharField(max_length=45)
     notes = models.CharField(max_length=255, default="note")
-    books = models.ManyToManyField(Book, related_name='publishers')
+    books = models.ManyToManyField(Book, related_name="publishers")
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
+
