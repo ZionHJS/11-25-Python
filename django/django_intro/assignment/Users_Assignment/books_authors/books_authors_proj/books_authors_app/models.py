a6eb448a89728c827bf24cf2326e403c9672f026
zionHuang <hjszion@gmail.com>
2019-12-04 18:11:15 -0800

    update

--- django/django_intro/assignment/Users_Assignment/books_authors/books_authors_proj/books_authors_app/models.py
+++ django/django_intro/assignment/Users_Assignment/books_authors/books_authors_proj/books_authors_app/models.py
@@ -3,12 +3,14 @@ from django.db import models
 # Create your models here.
 class Book(models.Model):
     title = models.CharField(max_length=255)
-    desc_TEXT = models.TextField()
+    desc_TEXT = models.TextField(default=default)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
 class Author(models.Model):
     first_name = models.CharField(max_length=45)
     last_name = models.CharField(max_length=45)
+    books = models.ManyToManyField(Book, related_name='publishers')
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
+    
