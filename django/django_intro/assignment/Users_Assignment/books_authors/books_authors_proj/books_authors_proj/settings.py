ac1ad47920e29e26ee8cab344dee3a2deeae0d63
zionHuang <hjszion@gmail.com>
2019-12-04 17:59:14 -0800

    update

--- django/django_intro/assignment/Users_Assignment/books_authors/books_authors_proj/books_authors_proj/settings.py
+++ django/django_intro/assignment/Users_Assignment/books_authors/books_authors_proj/books_authors_proj/settings.py
@@ -31,7 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
-    'books_author_app',
+    'books_authors_app',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
