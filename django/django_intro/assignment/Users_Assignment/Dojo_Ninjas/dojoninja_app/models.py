10ed00694b93aba776e77666e6877ab24f6ff449
zionHuang <hjszion@gmail.com>
2019-12-04 16:59:17 -0800

    update

--- django/django_intro/assignment/Users_Assignment/Dojo_Ninjas/dojoninja_app/models.py
+++ django/django_intro/assignment/Users_Assignment/Dojo_Ninjas/dojoninja_app/models.py
@@ -9,7 +9,7 @@ class Dojo(models.Model):
     update_at = models.DateTimeField(auto_now=True)
 
 class Ninja(models.Model):
-     DOJO = 
+     DOJO = models.ForeignKey(Dojo, related_name='ninjas', on_delete = models.CASCADE)
      first_name = models.CharField(max_length=255)
      last_name = models.CharField(max_length=255)
      create_at = models.DateTimeField(auto_now_add=True)
