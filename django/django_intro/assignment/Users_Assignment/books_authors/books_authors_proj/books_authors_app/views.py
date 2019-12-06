a6aee02b66825c8e51c3e6523800b7bff58b7f32
zionHuang <hjszion@gmail.com>
2019-12-05 16:17:20 -0800

    update

--- django/django_intro/assignment/Users_Assignment/books_authors/books_authors_proj/books_authors_app/views.py
+++ django/django_intro/assignment/Users_Assignment/books_authors/books_authors_proj/books_authors_app/views.py
@@ -1,5 +1,16 @@
 from django.shortcuts import render
+from .models import Book, Author
 
 # Create your views here.
 def index(request):
-    return render(request, 'index.html')
+    context={
+        'Books':Book.objects.all(),
+        'Authors':Author.objects.all()
+    }
+    return render(request, 'index.html', context)
+
+def show_book(request):
+    
+    context={
+        
+    }
