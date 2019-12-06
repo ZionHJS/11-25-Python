487e492818033ea4e1524cbf70ddb72526f1a595
zionHuang <hjszion@gmail.com>
2019-12-05 19:21:14 -0800

    update

--- django/django_intro/assignment/Users_Assignment/books_authors/books_authors_proj/books_authors_app/urls.py
+++ django/django_intro/assignment/Users_Assignment/books_authors/books_authors_proj/books_authors_app/urls.py
@@ -2,10 +2,14 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index),
+    path('', views.index_book),
     path('add_book', views.add_book),
     path('books/<int:id>', views.show_book),
     path('books/<int:id>/add_author2book', views.add_author2book),
+    path('authors', views.index_author),
+    path('add_author', views.add_author),
+    path('authors/<int:id>', views.show_author),
+    path('authors/<int:id>/add_book2author', views.add_book2author),
 ]
 
 
--- django/django_intro/assignment/Users_Assignment/books_authors/books_authors_proj/books_authors_app/views.py
+++ django/django_intro/assignment/Users_Assignment/books_authors/books_authors_proj/books_authors_app/views.py
@@ -2,12 +2,12 @@ from django.shortcuts import render, redirect
 from .models import Book, Author
 
 # Create your views here.
-def index(request):
+def index_book(request):
     context={
         'Books': Book.objects.all(),
         'Authors': Author.objects.all(),
     }
-    return render(request, 'index.html', context)
+    return render(request, 'index_book.html', context)
 
 def add_book(reqeust):
     book_title = reqeust.POST['title']
@@ -27,9 +27,44 @@ def show_book(request, id):
     return render(request, 'books_view.html', context)
 
 def add_author2book(request, id):
+    #print(request.POST)
     select_tag = request.POST['author_select']
-    author_id = select_tag
-    author2add = Author.objects.get(id = author_id)
+    author2add = Author.objects.get(id = int(select_tag))
     this_book = Book.objects.get(id=id)
     this_book.publishers.add(author2add)
-    return redirect('/books/{{id}}')
+    return redirect(f'/books/{id}')
+
+#author part
+def index_author(request):
+    context={
+        'Books': Book.objects.all(),
+        'Authors': Author.objects.all(),
+    }
+    return render(request, 'index_author.html', context)
+
+def add_author(reqeust):
+    author_first_name = reqeust.POST['first_name']
+    author_last_name = reqeust.POST['last_name']
+    author_notes = reqeust.POST['notes']
+    Author.objects.create(first_name=author_first_name, last_name=author_last_name)
+    return redirect('/authors')
+
+def show_author(request, id):
+    this_author = Author.objects.get(id=id)
+    context={
+        'author_first_name': Author.objects.get(id=id).first_name,
+        'author_last_name':Author.objects.get(id=id).last_name,
+        'author_id': id,
+        'author_notes': Author.objects.get(id=id).notes,
+        'author_books': this_author.books.all(),
+        'Books': Book.objects.all(),
+    }
+    return render(request, 'author_view.html', context)
+
+def add_book2author(request, id):
+    #print(request.POST)
+    select_tag = request.POST['book_select']
+    book2add = Book.objects.get(id = int(select_tag))
+    this_author = Author.objects.get(id=id)
+    this_author.books.add(book2add)
+    return redirect(f'/authors/{id}')
