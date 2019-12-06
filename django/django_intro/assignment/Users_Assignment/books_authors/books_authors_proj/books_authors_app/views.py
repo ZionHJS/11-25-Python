ade83076899d14a43ae322ef5f2936d9dca4a30c
zionHuang <hjszion@gmail.com>
2019-12-05 16:25:04 -0800

    update

--- django/django_intro/assignment/Users_Assignment/books_authors/books_authors_proj/books_authors_app/views.py
+++ django/django_intro/assignment/Users_Assignment/books_authors/books_authors_proj/books_authors_app/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import Book, Author
 
 # Create your views here.
@@ -9,8 +9,14 @@ def index(request):
     }
     return render(request, 'index.html', context)
 
+def add_book(reqeust):
+    book_title = reqeust.POST['title']
+    book_des = reqeust.POST['book_des']
+    Book.objects.create(title=book_title, desc_TEXT=book_des)
+    return redirect('/')
+
 def show_book(request):
-    
     context={
         
     }
+    return render(request, 'books_view.html')
