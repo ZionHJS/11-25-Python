e02fd4c4cc27ab53defc0a65921f1d4e3ef0c6d6
zionHuang <hjszion@gmail.com>
2019-12-06 18:32:50 -0800

    update

--- django/django_intro/assignment/Blog_Validation/blog_validation_proj/blog_validation_app/models.py
+++ django/django_intro/assignment/Blog_Validation/blog_validation_proj/blog_validation_app/models.py
@@ -1,5 +1,6 @@
 from __future__ import unicode_literals
 from django.db import models
+import re 
 
 class Blog(models.Model):
     name = models.CharField(max_length=255)
@@ -16,4 +17,13 @@ class BlogManager(models.Manager):
             errors["name"] = "Blog name should be at least 5 characters"
         if len(postData['desc']) < 10:
             errors["desc"] = "Blog description should be at least 10 characters"
+        return errors
+
+#using regex to validate
+class BlogManager_reg(models.Manager):
+    def basic_validator(self, postData):    
+        errors = {}
+        EMAIL_REGEX = re.compile(r'^[a-zA-Z0-9.+_-]+@[a-zA-Z0-9._-]+\.[a-zA-Z]+$')
+        if not EMAIL_REGEX.match(postData['email']):    # test whether a field matches the pattern            
+            errors['email'] = ("Invalid email address!")
         return errors
