38bd3658f6c67032aeda82a3a0bab6898cc187a7
zionHuang <hjszion@gmail.com>
2019-12-06 16:22:06 -0800

    update

--- django/django_intro/assignment/semi_restful_tv_shows/restfull_tvshows_proj/restfull_tvshows_app/urls.py
+++ django/django_intro/assignment/semi_restful_tv_shows/restfull_tvshows_proj/restfull_tvshows_app/urls.py
@@ -5,4 +5,6 @@ urlpatterns = [
     path('', views.index, name='index'),
     path('shows/new', views.shows_new),
     path('shows/<int:id>', views.shows_new),
+    path('shows/<int:id>/edit', views.edit_tvshow),
+    path('shows/<int:id>/delete', views.delete_tvshow),
 ]
--- django/django_intro/assignment/semi_restful_tv_shows/restfull_tvshows_proj/restfull_tvshows_app/views.py
+++ django/django_intro/assignment/semi_restful_tv_shows/restfull_tvshows_proj/restfull_tvshows_app/views.py
@@ -5,4 +5,13 @@ def index(request):
     return render(request, 'index.html')
     
 def shows_new(request):
+    #not done
     return render(request, 'shows_new.html')
+
+def edit_tvshow(request):
+    #not done
+    return render(request, 'edit_show.html')
+
+def delete_tvshow(request):
+    #not done
+    return redirect('/shows/new')
