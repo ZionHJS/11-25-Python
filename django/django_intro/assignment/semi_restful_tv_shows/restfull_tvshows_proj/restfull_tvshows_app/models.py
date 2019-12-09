7b88b2d08736a6edf13a1dccfc2e50d9c315f4ab
zionHuang <hjszion@gmail.com>
2019-12-09 14:50:26 -0800

    update

--- django/django_intro/assignment/semi_restful_tv_shows/restfull_tvshows_proj/restfull_tvshows_app/models.py
+++ django/django_intro/assignment/semi_restful_tv_shows/restfull_tvshows_proj/restfull_tvshows_app/models.py
@@ -1,6 +1,6 @@
 from __future__ import unicode_literals
 from django.db import models
-from datetime import datetime, timedelta
+from datetime import datetime
 
 # Create your models here.
 class ShowManager(models.Manager):
@@ -12,9 +12,10 @@ class ShowManager(models.Manager):
             errors['add_network'] = 'Network needs be at least 2 characters'
         if len(postData['add_des']) <= 10 and len(postData['add_des']) >= 0:
             errors['add_des'] = 'Description needs be at least 10 characters'
-        ticks = datetime.now()
-        if postData['add_date']
-            
+        nowtime = datetime.now()
+        if postData['add_date']:
+            if nowtime - postData['add_date'] <= 0:
+                errors['add_date'] = 'Date-time needs be in the past'
         return errors
 
 class Show(models.Model):
