93529c87486f3d283ca7aac101f65ef0483d59f8
zionHuang <hjszion@gmail.com>
2019-12-09 12:11:33 -0800

    update

--- django/django_intro/assignment/semi_restful_tv_shows/restfull_tvshows_proj/restfull_tvshows_app/models.py
+++ django/django_intro/assignment/semi_restful_tv_shows/restfull_tvshows_proj/restfull_tvshows_app/models.py
@@ -8,3 +8,10 @@ class Show(models.Model):
     description = models.CharField(max_length=255)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
+    objects = ShowManager()
+
+class ShowManager(models.manager):
+    def basic_validator(self, postData):
+        errors:{}
+        if len(postData.title) < 2:
+            errors['name'] = 'Title name needs be at least 2 characters'
