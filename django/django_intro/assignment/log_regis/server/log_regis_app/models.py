a4e01839207f9ea3691177c156754cc2e8b955dc
zionHuang <hjszion@gmail.com>
2019-12-10 13:30:02 -0800

    UPDATE

--- django/django_intro/assignment/log_regis/server/log_regis_app/models.py
+++ django/django_intro/assignment/log_regis/server/log_regis_app/models.py
@@ -1,11 +1,19 @@
 from django.db import models
 
 # Create your models here.
+class UserManager(models.Manager):
+    def basic_validator(self, postData):
+        errors = {}
+        if len(postData['first_name']) < 2:
+            errors['first_name'] = " First Name should be at least 2 characters and letters only!"
+        if len(postData['last_name']) <2:
+            errors['last_name'] = "Last Name should be at least 2 characters and letters only!"
+
 class User(models.Model):
     first_name = models.CharField(max_length=45)
     last_name = models.CharField(max_length=45)
     email = models.CharField(max_length=125)
     password = models.CharField(max_length=255)
-    password_confirm = models.CharField(max_length=255)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
+    objects = UserManager
