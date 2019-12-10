a8ee79814cb042215ff69e34bcd133b0a35abdf0
zionHuang <hjszion@gmail.com>
2019-12-09 17:28:48 -0800

    update

--- django/django_intro/assignment/courses/server/courses_app/views.py
+++ django/django_intro/assignment/courses/server/courses_app/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render, redirect
-from .models import Course
 from django.contrib import messages
+from .models import Course
 
 # Create your views here.
 def index(request):
@@ -12,17 +12,18 @@ def index(request):
 
 def add_course(request):
     errors = Course.objects.basic_validators(request.POST)
-    
-
-
-    course_name = request.POST['add_name']
-    course_des = request.POST['add_des']
-    Course.objects.create(course_name=course_name, description=course_des)
-    context = {
-        'courses':Course.objects.all()
-    }
-    return render(request, 'index.html', context)
-    #return redirect('/')
+    if len(errors) > 0:
+        for key, value in errors.items():
+            messages.error(request, value)
+        return redirect('/')
+    else:
+        course_name = request.POST['add_name']
+        course_des = request.POST['add_des']
+        Course.objects.create(course_name=course_name, description=course_des)
+        context = {
+            'courses':Course.objects.all()
+        }
+        return render(request, 'index.html', context)
 
 def show_remove(request, id):
     remove_obj = Course.objects.get(id = id)
