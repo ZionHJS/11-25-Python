090a9977266c81be663df5073679f9bc146dfb48
zionHuang <hjszion@gmail.com>
2019-12-10 14:16:23 -0800

    update

--- django/django_intro/assignment/log_regis/server/log_regis_app/models.py
+++ django/django_intro/assignment/log_regis/server/log_regis_app/models.py
@@ -12,6 +12,8 @@ class UserManager(models.Manager):
         EMAIL_REGEX = re.compile(r'^[a-zA-Z0-9.+_-]+@[a-zA-Z0-9._-]+\.[a-zA-Z]+$')
         if not EMAIL_REGEX.match(postData['email']):
             errors['email'] = ("Invalid email address!")
+        if len(postData['password']) < 8:
+            errors['password'] = "Password should be at least 8 characters!"
         return errors
 
 class User(models.Model):
--- django/django_intro/assignment/log_regis/server/log_regis_app/views.py
+++ django/django_intro/assignment/log_regis/server/log_regis_app/views.py
@@ -1,11 +1,30 @@
 from django.shortcuts import render, redirect
+from django.contrib import messages
+from .models import User
+import bcrypt
 
 # Create your views here.
 def index(request):
     return render(request, 'index.html')
 
 def regisration(request):
-    return redirect('success')
+    errors = User.objects.basic_validator(request.POST)
+    if len(errors) > 0:
+        for key, value in errors.items():
+            messages.error(request, value)
+        return redirect('/')
+    elif request.POST['password_confirm'] != request.POST['password']:
+        messages.error(request, 'The password entered twice must be the same!')
+    else:
+        first_name = request.POST['first_name']
+        last_name = request.POST['last_name']
+        email = request.POST['email']
+        password = request.POST['password']
+        user_pwd = bcrypt.hashpw(password.encode(), bcrypt.gensalt()).decode
+        this_user = User.objects.create(first_name=first_name, last_name=last_name, email=email, password=user_pwd)
+        messages.success(request, "Register successfully!")
+        return redirect('/success')
 
 def success(request):
+    
     return render(request, 'success.html')
