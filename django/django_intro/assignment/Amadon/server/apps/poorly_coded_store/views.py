f884e9fb80c63b5864bd6538f95b19940836cac4
zionHuang <hjszion@gmail.com>
2019-12-09 19:13:40 -0800

    update

--- django/django_intro/assignment/Amadon/server/apps/poorly_coded_store/views.py
+++ django/django_intro/assignment/Amadon/server/apps/poorly_coded_store/views.py
@@ -13,8 +13,16 @@ def checkout(request):
     total_charge = quantity_from_form * price_from_form
     print("Charging credit card...")
     this_order = Order.objects.create(quantity_ordered=quantity_from_form, total_price=total_charge)
+    all_orders = Order.objects.all()
+    total_quantity = 0
+    total_price = 0
+    for item in all_orders:
+        total_quantity += item.quantity_ordered
+        total_price += item.total_price
     context = {
-        'orders':Order.objects.all(),
-        'this_order':this_order
+        'orders':all_orders,
+        'this_order':this_order,
+        'total_quantity':total_quantity,
+        'total_price':total_price
     }
     return render(request, "store/checkout.html", context)
