6dbf5111f4484594795e5ccff2529bed87eff0e7
zionHuang <hjszion@gmail.com>
2019-12-11 10:16:45 -0800

    update

--- django/django_intro/assignment/FavoriteBooks/server/favorite_book_app/urls.py
+++ django/django_intro/assignment/FavoriteBooks/server/favorite_book_app/urls.py
@@ -2,5 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='index'),
+    path('', views.index),
+    path('registration', views.regisration),
+    path('login', views.login),
+    path('books', views.books),
 ]
--- django/django_intro/assignment/FavoriteBooks/server/favorite_book_app/views.py
+++ django/django_intro/assignment/FavoriteBooks/server/favorite_book_app/views.py
@@ -1,6 +1,56 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib import messages
+from .models import User
+import bcrypt
 
 # Create your views here.
 def index(request):
     return render(request, 'index.html')
 
+def regisration(request):
+    errors = User.objects.basic_validator(request.POST)
+    if len(errors) > 0:
+        for key, value in errors.items():
+            messages.error(request, value)
+        return redirect('/')
+    elif request.POST['password_confirm'] != request.POST['password']:
+        messages.error(request, 'The password entered twice must be the same!')
+    else:
+        first_name = request.POST['first_name']
+        last_name = request.POST['last_name']
+        email = request.POST['email']
+        password = request.POST['password']
+        user_pwd = bcrypt.hashpw(password.encode(), bcrypt.gensalt()).decode()
+        this_user = User.objects.create(first_name=first_name, last_name=last_name, email=email, password=user_pwd)
+        request.session['this_user_id'] = this_user.id
+        messages.success(request, "Register successfully!")
+        return redirect('/books')
+
+def login(request):
+    found_user = User.objects.filter(email=request.POST['email'])
+    if len(found_user) < 1:
+        messages.error(request, 'Invalid Credentials')
+        return redirect('/')
+    else:
+        logged_user = found_user[0]
+        if bcrypt.checkpw(request.POST['password'].encode(), logged_user.password.encode()):
+            request.session['this_user_id'] = logged_user.id
+            return redirect('/books')
+        else:
+            messages.error(request, 'Invalid Credentials')
+            return redirect('/')
+
+def books(request):
+    this_id = request.session.get('this_user_id')
+    if this_id is None:
+        return redirect('/')
+    else:
+        this_user = User.objects.get(id = this_id)
+        context={
+            "this_user":this_user,
+        }
+        return render(request, 'success.html', context)
+
+def logout(request):
+    request.session.clear()
+    return redirect('/')
