0d0adda465539dab50567be186b1aaee2d2e2b17
zionHuang <hjszion@gmail.com>
2019-12-12 15:04:51 -0800

    update

--- django/django_intro/assignment/UserDashboard/server/user_table_app/models.py
+++ django/django_intro/assignment/UserDashboard/server/user_table_app/models.py
@@ -5,13 +5,13 @@ import re
 class UserManager(models.Manager):
     def basic_validator(self, postData):
         errors = {}
+        EMAIL_REGEX = re.compile(r'^[a-zA-Z0-9.+_-]+@[a-zA-Z0-9._-]+\.[a-zA-Z]+$')
+        if not EMAIL_REGEX.match(postData['email']):
+            errors['email'] = ("Invalid email address!")
         if len(postData['first_name']) < 2:
             errors['first_name'] = " First Name should be at least 2 characters and letters only!"
         if len(postData['last_name']) <2:
             errors['last_name'] = "Last Name should be at least 2 characters and letters only!"
-        EMAIL_REGEX = re.compile(r'^[a-zA-Z0-9.+_-]+@[a-zA-Z0-9._-]+\.[a-zA-Z]+$')
-        if not EMAIL_REGEX.match(postData['email']):
-            errors['email'] = ("Invalid email address!")
         if len(postData['password']) < 8:
             errors['password'] = "Password should be at least 8 characters!"
         return errors
--- django/django_intro/assignment/UserDashboard/server/user_table_app/views.py
+++ django/django_intro/assignment/UserDashboard/server/user_table_app/views.py
@@ -3,3 +3,7 @@ from django.shortcuts import render, redirect
 # Create your views here.
 def index(request):
     return render(request, 'index.html')
+
+def signin(request):
+    
+    return render(request, 'signin.html')
