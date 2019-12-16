60b133cb869be8d70f8caee41fbb16d4d4c53eef
zionHuang <hjszion@gmail.com>
2019-12-16 14:23:08 -0800

    update

--- django/django_intro/assignment/AfterExam_Project/server/clock_in_out_app/urls.py
+++ django/django_intro/assignment/AfterExam_Project/server/clock_in_out_app/urls.py
@@ -4,4 +4,6 @@ from . import views
 urlpatterns = [
     path('', views.index),
     path('login', views.login),
+    path('login_verify', views.login_verify),
+    path('register', views.register)
 ]
--- django/django_intro/assignment/AfterExam_Project/server/clock_in_out_app/views.py
+++ django/django_intro/assignment/AfterExam_Project/server/clock_in_out_app/views.py
@@ -6,3 +6,41 @@ def index(request):
 
 def login(request):
     return render(request, 'login.html')
+
+def register(request):
+    return render(request, 'register.html')
+
+def login_verify(request):
+    found_user = User.objects.filter(email=request.POST['email_address'])
+    if len(found_user) < 1:
+        messages.error(request, 'Invalid Credentials')
+        return redirect('/')
+    else:
+        logged_user = found_user[0]
+        if bcrypt.checkpw(request.POST['password'].encode(), logged_user.password.encode()):
+            request.session['this_user_id'] = logged_user.id
+            return redirect('/dashboard')
+        else:
+            messages.error(request, 'Invalid Credentials')
+            return redirect('/')
+
+def register(request):
+    errors = User.objects.basic_validator(request.POST)
+    if len(errors) > 0:
+        for key, value in errors.items():
+            messages.error(request, value)
+        return redirect('/')
+    elif request.POST['password_confirm'] != request.POST['password']:
+        messages.error(request, 'The password entered twice must be the same!')
+    else:
+        email = request.POST['email']
+        first_name = request.POST['first_name']
+        last_name = request.POST['last_name']
+        password = request.POST['password']
+        user_pwd = bcrypt.hashpw(password.encode(), bcrypt.gensalt()).decode()
+        this_user = User.objects.create(email=email, first_name=first_name, last_name=last_name, password=user_pwd)
+        request.session['this_user_id'] = this_user.id
+        first_user = User.objects.first()
+        first_user.user_level = 9
+        messages.success(request, "Register successfully!")
+        return redirect('/dashboard')
